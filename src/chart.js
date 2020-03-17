0cb0ae471254ba38cf95e388162a657eb85c0f74
Viliam Rockai <viliam.rockai@gmail.com>
2020-03-17 19:26:42 +0100

    compare sk vs cz

--- src/chart.js
+++ src/chart.js
@@ -58,6 +58,54 @@ function drawChart(chartNode, timeAxis, dataConfirmed, dataRecovered, dataDeaths
   })
 }
 
+function drawChart2(chartNode, timeAxis, label1, dataConfirmed1, label2, dataConfirmed2) {
+  const gridLines = {
+    color: '#323232',
+    lineWidth: 2,
+    borderDash: [5, 5],
+  };
+
+  const ticks = {
+    fontColor: '#808080',
+  };
+
+  return chartNode.drawChart({
+    type: 'line',
+    data: {
+      labels: timeAxis,
+      datasets: [{
+        label: label1,
+        backgroundColor: '#6dad42',
+        borderColor: '#6dad42',
+        data: dataConfirmed1,
+        borderWidth: 4,
+        fill: false,
+      }, {
+        label: label2,
+        data: dataConfirmed2,
+        backgroundColor: '#ffffff',
+        borderColor: '#ffffff',
+        borderWidth: 4,
+        fill: false,
+      }]
+    },
+    options: {
+      scales: {
+        xAxes: [{gridLines, ticks}],
+        yAxes: [{gridLines, ticks}],
+      },
+      legend: {
+        display: true,
+        labels: {
+          fontColor: '#ffffff',
+          fontSize: 16
+        },
+      }
+    },
+  })
+}
+
 module.exports = {
-  drawChart
+  drawChart,
+  drawChart2
 };
--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ require('jsdom');
 
 const env = process.env;
 const ChartjsNode = require('chartjs-node');
-const {drawChart} = require('./chart');
+const {drawChart, drawChart2} = require('./chart');
 const {getCsvRowPromise} = require('./csv');
 
 const width = env['COR_WIDTH'] || 1200;
 const height = env['COR_HEIGHT'] || 600;
 const country = env['COR_COUNTRY'] || 'Slovakia';
 
-
 const confirmed$ = getCsvRowPromise('./time_series_19-covid-Confirmed.csv', country);
 const deaths$ = getCsvRowPromise('./time_series_19-covid-Deaths.csv', country);
 const recovered$ = getCsvRowPromise('./time_series_19-covid-Recovered.csv', country);
+
+const population = {
+  'Slovakia': 5441899,
+  'Czechia': 10649800,
+};
 
 Promise.all([confirmed$, deaths$, recovered$]).then(
   ([[timeAxis, dataConfirmed], [, dataDeaths], [, dataRecovered]]) => {
@@ -25,6 +29,25 @@ Promise.all([confirmed$, deaths$, recovered$]).then(
   }
 );
 
+const country1 = 'Slovakia';
+const country2 = 'Czechia';
 
+const confirmed1$ = getCsvRowPromise('./time_series_19-covid-Confirmed.csv', country1);
+const confirmed2$ = getCsvRowPromise('./time_series_19-covid-Confirmed.csv', country2);
 
+Promise.all([confirmed1$, confirmed2$]).then(
+  ([[timeAxis, dataConfirmed1], [, dataConfirmed2]]) => {
+      const chartNode = new ChartjsNode(width, height);
 
+      const perPerson1 = n => n / population[country1];
+      const dataConfirmedPp1 = dataConfirmed1.map(perPerson1);
+
+      const perPerson2 = n => n / population[country2];
+      const dataConfirmedPp2 = dataConfirmed2.map(perPerson2);
+
+      drawChart2(chartNode, timeAxis, country1, dataConfirmedPp1, country2, dataConfirmedPp2)
+        .then(() => chartNode.getImageBuffer('image/png'))
+        .then(buffer => chartNode.getImageStream('image/png'))
+        .then(streamResult => chartNode.writeImageToFile('image/png', './docs/corona_stats_compare.png'));
+  }
+);
